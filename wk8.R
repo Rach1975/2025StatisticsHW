34869e7152b76ebd153f1fe7e46f129579302799
Rach1975 <stwzhang2022@gmail.com>
2025-04-06 10:58:26 +0800

    完成excel数据整理；创建项目和数据存放、代码存放；将excel导入R Studio

---
+++ wk8.R
@@ -0,0 +1,18 @@
+
+# 准备 ----------------------------------------------------------------------
+
+# installation - skip it if done before
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("readxl")
+
+library(ggplot2)
+library(dplyr)
+library(readxl)
+
+
+# 导入数据并分析数据 -----------------------------------------------------------------
+
+data <- read_excel("bank-additional.xlsx")
+
+
